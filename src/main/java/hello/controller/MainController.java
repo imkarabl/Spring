09e40c9546ac49d17c172d2cb397011c0f86374d
Imkarabl <imkarabl96@gmail.com>
2018-11-01 15:23:16 +0300

    Не запускается!!!!!!!!!!!!!!!!

--- src/main/java/hello/controller/MainController.java
+++ src/main/java/hello/controller/MainController.java
@@ -32,7 +32,7 @@ public class MainController {
     }
     @PostMapping("main")
     public String add(@AuthenticationPrincipal User user, @RequestParam String text, @RequestParam String tag, Map<String, Object> model){
-    Message message = new Message(text,tag);
+    Message message = new Message(text,tag, user);
 
     messageRepo.save(message);
 
